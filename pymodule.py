02b821cb34eb8751f39fb6fd94bfa1cde45e1768
Robert McGibbon <rmcgibbo@gmail.com>
2015-04-17 18:01:49 -0700

    Getting used to this

--- pymodule.py
+++ pymodule.py
@@ -10,29 +10,29 @@ from molutil import *
 import p4util
 from psiexceptions import *
 
+#
+# def run_wavekernel(name, **kwargs):
+#     r"""Function encoding sequence of PSI module and plugin calls so that
+#     wavekernel can be called via :py:func:`~driver.energy`. For post-scf plugins.
+#
+#     >>> energy('wavekernel')
+#
+#     """
+#     lowername = name.lower()
+#     kwargs = p4util.kwargs_lower(kwargs)
+#
+#     # Your plugin's psi4 run sequence goes here
+#     psi4.set_global_option('BASIS', 'sto-3g')
+#     psi4.set_local_option('WAVEKERNEL', 'PRINT', 1)
+#     scf_helper(name, **kwargs)
+#     returnvalue = psi4.plugin('wavekernel.so')
+#     psi4.set_variable('CURRENT ENERGY', returnvalue)
+#
+#
+# # Integration with driver routines
+# # procedures['energy']['wavekernel'] = run_wavekernel
+#
 
-def run_wavekernel(name, **kwargs):
-    r"""Function encoding sequence of PSI module and plugin calls so that
-    wavekernel can be called via :py:func:`~driver.energy`. For post-scf plugins.
-
-    >>> energy('wavekernel')
-
-    """
-    lowername = name.lower()
-    kwargs = p4util.kwargs_lower(kwargs)
-
-    # Your plugin's psi4 run sequence goes here
-    psi4.set_global_option('BASIS', 'sto-3g')
-    psi4.set_local_option('WAVEKERNEL', 'PRINT', 1)
-    scf_helper(name, **kwargs)
-    returnvalue = psi4.plugin('wavekernel.so')
-    psi4.set_variable('CURRENT ENERGY', returnvalue)
-
-
-# Integration with driver routines
-procedures['energy']['wavekernel'] = run_wavekernel
-
-
-def exampleFN():
-    # Your Python code goes here
-    pass
+def sample_descriptors(n=100):
+    psi4.set_local_option('WAVEKERNEL', 'MODE', 'SAMPLE_DESCRIPTORS')
+    psi4.plugin('wavekernel.so')
--- wavekernel.cc
+++ wavekernel.cc
@@ -7,29 +7,44 @@
 
 INIT_PLUGIN
 
+using std::string;
 using namespace boost;
 
 namespace psi{ namespace wavekernel {
 
-extern "C" 
+extern "C"
 int read_options(std::string name, Options& options)
 {
     if (name == "WAVEKERNEL"|| options.read_globals()) {
         /*- The amount of information printed to the output file -*/
         options.add_int("PRINT", 1);
+        options.add_str("MODE", "", "Wave kernel plugin mode");
     }
 
     return true;
 }
 
-extern "C" 
+void sample_descriptors() {
+    fprintf(outfile, "WAVE KERNEL PLUGIN\n");
+    shared_ptr<Molecule> molecule = Process::environment.molecule();
+}
+
+
+extern "C"
 PsiReturnType wavekernel(Options& options)
 {
     int print = options.get_int("PRINT");
+    string mode = options.get_str("MODE");
+    if (mode == "SAMPLE_DESCRIPTORS") {
+        sample_descriptors();
+        return Success;
+    } else {
+        return Failure;
+    }
 
-    /* Your code goes here */
 
-    return Success;
+
+
 }
 
 }} // End namespaces
